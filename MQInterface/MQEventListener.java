9ddff2fc14548e18323bdfa88f1138dc9f66833e
tender4ever <tender4ever.young@gmail.com>
2017-05-16 15:46:19 +0800

    Delete MQEventListener.java

--- MQInterface/MQEventListener.java
+++
@@ -1,9 +0,0 @@
-package MQInterface;
-
-import java.util.*;
-
-public interface MQEventListener extends EventListener{
-
-	public void onMessage(String txtMessage);
-	public void onConnect(String status);
-}
