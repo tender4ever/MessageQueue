081eb195bf69ae9b2fae800d1da7702a4087d490
tender4ever <tender4ever.young@gmail.com>
2017-05-16 15:46:42 +0800

    Delete MQReceiver.java

--- MQInterface/MQReceiver.java
+++
@@ -1,9 +0,0 @@
-package MQInterface;
-
-public interface MQReceiver {
-
-	public void connect();
-	public void disconnect();
-	public String getConnectionStatus();
-	public void setConnectionStatus(String connectionState);
-}
